bbeba54bdecac3e62a50e4368aad1237a1ce8d75
John Nazarene Dela Pisa <jannazarene09@gmail.com>
2025-12-01 02:28:22 +0000

    feat: add Microsoft Clarity tracking script and update analytics interface

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,6 +57,20 @@ export default function RootLayout({
             __html: JSON.stringify(PROFILE_PAGE_SCHEMA),
           }}
         />
+        {/* Microsoft Clarity */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "uehtex8zqz");
+            `,
+          }}
+        />
 
         {/* Apple iOS Safari specific */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
--- app/utils/analytics.ts
+++ app/utils/analytics.ts
@@ -4,6 +4,8 @@ declare global {
     beacon: (opts: any) => void;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     gtag: (...args: any[]) => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    clarity: (...args: any[]) => void;
   }
 }
 
